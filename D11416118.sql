4698df6e007eebed36700a2f1e47ba0319681c5f
d11416118-sketch <d11416118@gs.takming.edu.tw>
2026-09-21 11:50:13 +0800

    看學號的就好

--- D11416118.sql
+++ D11416118.sql
@@ -52,7 +52,7 @@ from D11416118_1
 
 
 select *
-from  TO_SQL
+from  [TO_SQL$]			--從excel匯入的
 
 
 --07.篩選表格資料  條件1
@@ -71,14 +71,57 @@ where FirstName = 'Milk' and LastName = 'Schmidt1'
 
 --09.篩選表格資料  條件3
            --like   %
+select * 
+from	D11416118_1
+where FirstName = 'Milk' and LastName like 'Sc%'
 
+select * 
+from	D11416118_1
+where FirstName = 'Milk' and LastName like 'Schmidt%'
 
+-- 上下結果一樣  like 有就符合條件會出現
 
 --10.篩選表格資料  條件4
           -- _
-
+select * 
+from	D11416118_1
+where FirstName = 'Milk' and LastName like 'Schmidt_'
 
 
 --11.篩選表格資料  條件5
 
+
+
+
+--12.  更改儲存格內容  update	set
+
+select *
+from D11416118_1
+
+update D11416118_1 set age = 118
+where FirstName = '張' and LastName = '富富'
+
+-- 13. 刪除表格 delete
+
+delete  D11416118_1$	
+select *	--	顯示資料
+from D11416118_1
+
+
+-- 14. 刪除表格內的列(row) 
+delete  D11416118_1
+where FirstName = 'Milk' and LastName = 'Schmidt5'
+
+
+select *	--	顯示資料
+from D11416118_1
+
+
+
+--15. 備註	-- 列  /**/區塊
+ -- 一列 
+ /*
+	我是第一行
+	我是區塊 
+ */
 
